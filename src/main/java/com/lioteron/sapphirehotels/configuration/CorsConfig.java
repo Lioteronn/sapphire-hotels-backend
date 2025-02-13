b8125609d6314f71c4bc341106d6f6359a0f0fb0
Lioteronn <lioteronex@gmail.com>
2025-02-13 13:11:07 +0100

    Added jar file

--- src/main/java/com/lioteron/sapphirehotels/configuration/CorsConfig.java
+++ src/main/java/com/lioteron/sapphirehotels/configuration/CorsConfig.java
@@ -9,10 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5174")
+                .allowedOrigins("https://sapphire-hotels-main.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowCredentials(true);
     }
 }
